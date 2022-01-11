1358000571ee7b5542d10c97cc53dc2c269ba41b
Baris0 <barisseckin0@gmail.com>
2022-01-11 18:09:09 +0300

    delete

--- src/main/java/com/finartz/flightTicketSytem/dataAccess/abstracts/TicketDao.java
+++
@@ -1,8 +0,0 @@
-package com.finartz.flightTicketSytem.dataAccess.abstracts;
-
-import com.finartz.flightTicketSytem.entities.concretes.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketDao extends JpaRepository<Ticket, String> {
-    Ticket getByticketNumber(String ticketNumber);
-}
